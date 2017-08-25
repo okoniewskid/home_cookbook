8126d06f63b72bdfc23904dabc90fea0fb8f94e0
Daniel <okoniewskid@gmail.com>
2017-08-25 19:57:44 +0200

    Added slack installation

--- recipes/default.rb
+++ recipes/default.rb
@@ -9,3 +9,10 @@ include_recipe 'chocolatey::default'
 
 # Install Chocolatey dependant packages
 include_recipe 'home_cookbook::chocolatey_packages'
+
+# Installs slack from website
+windows_package 'slack' do
+  source 'https://slack.com/ssb/download-win'
+  action :install
+  installer_type :inno
+end
