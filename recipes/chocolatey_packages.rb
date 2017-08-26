07c3d48f3978f6ed6105805ac88e8b10eb670fe3
Daniel <okoniewskid@gmail.com>
2017-08-26 11:05:22 +0200

    Fixes
    Removed entertainment.rb as it's now worth the work to silently install
    steam and gog. Added timeout to chocolatey_packages

--- recipes/chocolatey_packages.rb
+++ recipes/chocolatey_packages.rb
@@ -1,23 +1,26 @@
 # Installs all software in array, in order.
-chocolatey_package %w[
-  googlechrome
-  7zip.install
-  git.install
-  putty.install
-  atom
-  vagrant
-  vagrant-manager
-  packer
-  docker
-  docker-compose
-  ruby
-  mpc-hc
-  ccleaner
-  curl
-  thunderbird
-  virtualbox
-  winscp
-  totalcommander
-  rufus
-  vim
-]
+chocolatey_package 'packages' do
+  package_name %w[
+    googlechrome
+    7zip.install
+    git.install
+    putty.install
+    atom
+    vagrant
+    vagrant-manager
+    packer
+    docker
+    docker-compose
+    ruby
+    mpc-hc
+    ccleaner
+    curl
+    thunderbird
+    virtualbox
+    winscp
+    totalcommander
+    rufus
+    vim
+  ]
+  timeout 3600
+end
--- recipes/default.rb
+++ recipes/default.rb
@@ -10,9 +10,6 @@ include_recipe 'chocolatey::default'
 # Install Chocolatey dependant packages
 include_recipe 'home_cookbook::chocolatey_packages'
 
-# Installs software for entertainment like games
-include_recipe 'home_cookbook::entertainment'
-
 # Installs slack from website
 windows_package 'slack' do
   source 'https://slack.com/ssb/download-win'
